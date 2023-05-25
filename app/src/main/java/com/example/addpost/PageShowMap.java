bb2b34ab053219dec180e4046319176eb21340d6
PPloyIXVII <63050154@kmitl.ac.th>
2023-05-25 15:30:22 +0700

    Search Map

--- app/src/main/java/com/example/addpost/PageShowMap.java
+++ app/src/main/java/com/example/addpost/PageShowMap.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.widget.FrameLayout;
+import android.widget.SearchView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,19 +18,58 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class PageShowMap extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap gMap;
-    private FrameLayout map;
+    private SearchView mapSearchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_show_map);
 
-        map = findViewById(R.id.map);
+        //map = findViewById(R.id.map);
+        mapSearchView = findViewById(R.id.mapSearch);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        //mapFragment.getMapAsync(this);
+
+        mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                String location = mapSearchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if(location != null){
+                    Geocoder geocoder = new Geocoder(PageShowMap.this);
+
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    gMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        mapFragment.getMapAsync(PageShowMap.this);
     }
 
     @Override
@@ -35,8 +77,8 @@ public class PageShowMap extends FragmentActivity implements OnMapReadyCallback 
         this.gMap = googleMap;
 
         //ใส่ตำแหน่งของ ประเทศไทย Thailand coordinates
-        LatLng mapThai = new LatLng(15.8700,100.9925);
-        this.gMap.addMarker(new MarkerOptions().position(mapThai).title("Marker in Thailand"));
-        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapThai));
+        //LatLng mapThai = new LatLng(15.8700,100.9925);
+        //this.gMap.addMarker(new MarkerOptions().position(mapThai).title("Marker in Thailand"));
+        //this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapThai));
     }
 }
